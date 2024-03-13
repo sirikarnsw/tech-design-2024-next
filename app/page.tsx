3552e954d5e1843f4a1f3da6af41b5b4720a2548
sirikarnsw <karn14673@gmail.com>
2024-03-13 16:12:43 +0700

    add rainbow

--- app/page.tsx
+++ app/page.tsx
@@ -64,13 +64,14 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+            Learn🌈{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
+            🌈🌈🌈🌈🌈🌈🌈🌈
           </p>
         </a>
 
@@ -88,6 +89,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore starter templates for Next.js.
+            I love coding so much lol 🥲
           </p>
         </a>
 
